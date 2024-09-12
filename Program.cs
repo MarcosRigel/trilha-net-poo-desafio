26a80cece4219d7491fa5585503dfdfda3bb78fd
Marcos Rigel <marcosrigel99@gmail.com>
2024-09-12 20:10:26 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone celularNokia = new Nokia("123456", "Modelo 1", "1111111111", 64);
+celularNokia.Ligar();
+celularNokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+Smartphone celularIphone = new Nokia("4987", "Modelo 2", "2222222222", 128);
+celularIphone.ReceberLigacao();
+celularIphone.InstalarAplicativo("Telegram");
